9af5d755d52b4930f22de9d06c0845d718955c28
ReyHanaFitria <rhan80888@gmail.com>
2024-10-15 22:43:42 +0700

    first commit

---
+++ petugas/index.php
@@ -0,0 +1,47 @@
+<?php
+include "header.php";
+include "navbar.php";
+?>
+<div class="card mt-2">
+    <div class="card-body">
+        <div class="row">
+        <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    Data Barang
+                    <h3>3</h3>
+<a href="data_barang.php" class="btn btn-outline-primary btn-sm">Detail</a>
+</div>
+</div>
+</div>
+<div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    Data Pembelian
+                    <h3>3</h3>
+<a href="pembelian.php" class="btn btn-outline-primary btn-sm">Detail</a>
+</div>
+</div>
+</div>
+<div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    Stok Barang
+                    <h3>3</h3>
+<a href="stok_barang.php" class="btn btn-outline-primary btn-sm">Detail</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="card mt-2">
+    <div class="card-body">
+        <p>selamat datang di halaman Petugas, silahkan anda bisa mengakses beberapa fitur</p>
+</div>
+</div>
+<?php
+include "footer.php";
+?>
